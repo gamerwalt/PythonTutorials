03a1f5cc759b9cb3e0f509b70ff9cdd5f6bdf4ed
Olawale Adewoyin <gamerwalt@gmail.com>
2021-04-13 17:28:55 +0000

    complete 2nd challenge

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 
 #day 1
-test = input("What is your name?")
-print(f"Length of your name is {len(test)}")
+#test = input("What is your name?")
+#print(f"Length of your name is {len(test)}")
+
+#day1 - 1st Challenge
+#a = input("a:")
+#b = input("b:")
+
+#temp = a
+#a = b
+#b = temp
+
+#print("a = " + a)
+#print("b = " + b)
+
+#day1 - 2nd Challenge
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("")
+print(f"Your band name could be {city} {pet}")
